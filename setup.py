ca9917edcc9ea1dc0f94ee3c4db92944d53b8f4b
David Black <david.black@colorado.edu>
2020-07-21 08:42:21 -0400

    Fixed wheel packaging logic so that it properly captures all the source packages

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 
-import setuptools
+from setuptools import find_packages
+from setuptools import setup
 
 _NAME='integrationutils'
 _VERSION='0.0.1'
@@ -12,16 +13,16 @@ _INSTALL_REQUIRES = [
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-        name=_NAME,
-        version=_VERSION,
-        author='David Black',
-        author_email='dblack1021@gmail.com',
-        description=_DESCRIPTION,
-        long_description=long_description,
-        long_description_content_type='text/markdown',
-        url='',
-        instal_requires=_INSTALL_REQUIRES,
-        packages=[NAME],
-        package_dir={'': 'src'}
-        )
+setup(
+    name=_NAME,
+    version=_VERSION,
+    author='David Black',
+    author_email='dblack1021@gmail.com',
+    description=_DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='',
+    install_requires=_INSTALL_REQUIRES,
+    packages=find_packages('src'),
+    package_dir={'': 'src'}
+)
