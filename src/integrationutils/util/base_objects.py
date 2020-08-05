7975fedc6f1f697536270064f7652c09db08c4ac
David Black <david.black@colorado.edu>
2020-08-04 23:12:24 -0400

    Updated matrix and vector implementations, added additional unit tests

--- src/integrationutils/util/base_objects.py
+++ src/integrationutils/util/base_objects.py
@@ -1,30 +1,164 @@
 from math import sqrt
-from typing import Union
+from typing import List
+from typing import Tuple
 
-class Vec3d:
+class Matrix:
+    """Class represents a matrix."""
+
+    @classmethod
+    def fill(cls, num_rows: int, num_cols: int, fill_value: float):
+        if (num_rows <= 0) or (num_cols <= 0):
+            raise ValueError("The number of rows or columns must be positive and greater than 0.")
+        fill_value = float(fill_value)
+        return cls(A=[[fill_value for _ in range(int(num_cols))] for _ in range(int(num_rows))])
+
+    @classmethod
+    def zeros(cls, num_rows: int, num_cols: int = None):
+        if not num_cols and num_cols != 0.0:
+            num_cols = num_rows
+        return Matrix.fill(num_rows, num_cols, 0.0)
+
+    @classmethod
+    def ones(cls, num_rows: int, num_cols: int = None):
+        if not num_cols and num_cols != 0.0:
+            num_cols = num_rows
+        return Matrix.fill(num_rows, num_cols, 1.0)
+
+    @classmethod
+    def identity(cls, dim: int):
+        A = Matrix.zeros(dim)
+        for i in range(dim):
+            A[i,i] = 1.0
+        return A
+
+    def __init__(self, A: List[List]):
+        self._num_rows = len(A) if A else 0
+        self._num_cols = len(A[0]) if A else 0
+        for row in A:
+            if len(row) != self._num_cols:
+                raise ValueError('Each row must have the same number of columns.')
+        self._A = A
+
+    def __str__(self) -> str:
+        def stringify_row(row: List):
+            return ", ".join([str(x) for x in row])
+        mat = "\n ".join(stringify_row(row) for row in self._A)
+        return f"[{mat}]"
+
+    def __setitem__(self, indices: Tuple[int,int], value: float):
+        self._A[indices[0]][indices[1]] = value
+
+    def __getitem__(self, indices: Tuple[int,int]) -> float:
+        return self._A[indices[0]][indices[1]]
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Matrix):
+            return False
+        if (self._num_rows != other._num_rows) or (self._num_cols != other._num_cols):
+            return False
+        for i in range(self._num_rows):
+            for j in range(self._num_cols):
+                if self[i,j] != other[i,j]:
+                    return False
+        return True
+
+    def __add__(self, other):
+        if not (isinstance(other, Matrix) or \
+                isinstance(other, float) or \
+                isinstance(other, int)):
+            return NotImplemented
+        if isinstance(other, float) or isinstance(other, int):
+            other = Matrix.fill(self._num_rows, self._num_cols, other)
+        if (self._num_rows != other._num_rows) or (self._num_cols != other._num_cols):
+            raise ValueError("Matrices must be the same size to be added.")
+        M = Matrix.zeros(self._num_rows, self._num_cols)
+        for i in range(self._num_rows):
+            for j in range(self._num_cols):
+                M[i,j] = self[i,j] + other[i,j]
+        return M
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        return NotImplemented
+
+    def __rsub__(self, other):
+        return NotImplemented
+
+    def __mult__(self, other):
+        return NotImplemented
+
+    def __rmult__(self, other):
+        return NotImplemented
+
+    def transpose(self):
+        raise NotImplementedError
+
+class Vec3d(Matrix):
     """Class represents a Euclidean vector."""
 
+    @classmethod
+    def zeros(cls):
+        return Vec3d(0,0,0)
+
+    @classmethod
+    def ones(cls):
+        return Vec3d(1,1,1)
+
+    @classmethod
+    def identity(cls, dim: int):
+        raise NotImplementedError()
+
+    @classmethod
+    def fill(cls, num_rows: int, num_cols: int, fill_value: float):
+        raise NotImplementedError()
+
     def __init__(self, x: float = 0.0, y: float = 0.0, z: float = 0.0):
-        self.x = x
-        self.y = y
-        self.z = z
+        super().__init__([[x],[y],[z]])
+
+    @property
+    def x(self) -> float:
+        return self[0,0]
+
+    @x.setter
+    def x(self, value: float):
+        self[0,0] = value
+
+    @property
+    def y(self) -> float:
+        return self[1,0]
+
+    @y.setter
+    def y(self, value: float):
+        self[1,0] = value
+
+    @property
+    def z(self) -> float:
+        return self[2,0]
+
+    @z.setter
+    def z(self, value: float):
+        self[2,0] = value
 
     def __str__(self) -> str:
         return f'[x = {self.x}, y = {self.y}, z = {self.z}]'
 
-    def __eq__(self, other) -> bool:
-        if not isinstance(other, Vec3d):
-            return NotImplemented
-        return (self.x == other.x and
-                self.y == other.y and
-                self.z == other.z)
-
     def __add__(self, other):
-        if not isinstance(other, Vec3d):
-            return NotImplemented
-        return Vec3d(x = self.x + other.x,
-                     y = self.y + other.y,
-                     z = self.z + other.z)
+        result = super().__add__(other)
+        return Vec3d(result[0,0], result[1,0], result[2,0])
+
+    def __radd__(self, other):
+        result = super().__radd__(other)
+        return Vec3d(result[0,0], result[1,0], result[2,0])
+
+    def __sub__(self, other):
+        result = super().__sub__(other)
+        return Vec3d(result[0,0], result[1,0], result[2,0])
+
+    def __rsub__(self, other):
+        result = super().__rsub__(other)
+        return Vec3d(result[0,0], result[1,0], result[2,0])
 
     def norm(self) -> float:
         """Returns the Euclidean norm of the calling vector."""
@@ -52,56 +186,3 @@ class Vec3d:
         if not isinstance(other, Vec3d):
             return NotImplemented
         return self.x * other.x + self.y * other.y + self.z * other.z
-
-
-class Matrix3d:
-    """Class represents a 3x3 (3 row, 3 column) matrix."""
-
-    @classmethod
-    def zero(cls):
-        return cls()
-
-    @classmethod
-    def identity(cls):
-        return cls(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0)
-
-    @classmethod
-    def fill(cls, value: float):
-        return cls(*([value]*9))
-
-    def __init__(self, M_11: float = 0.0, M_12: float = 0.0, M_13: float = 0.0,
-                       M_21: float = 0.0, M_22: float = 0.0, M_23: float = 0.0,
-                       M_31: float = 0.0, M_32: float = 0.0, M_33: float = 0.0):
-        self.M_11 = M_11
-        self.M_12 = M_12
-        self.M_13 = M_13
-        self.M_21 = M_21
-        self.M_22 = M_22
-        self.M_23 = M_23
-        self.M_31 = M_31
-        self.M_32 = M_32
-        self.M_33 = M_33
-
-    def __str__(self) -> str:
-        return f"[{self.M_11}, {self.M_12}, {self.M_13};\n{self.M_21}, {self.M_22}, {self.M_23};\n{self.M_31}, {self.M_32}, {self.M_33}]"
-
-    def __add__(self, other):
-        if not (isinstance(other, Matrix3d) or isinstance(other, float)):
-            return NotImplemented
-        if isinstance(other, float):
-            other = Matrix3d.fill(other)
-        pass
-
-    def __radd_(self, other):
-        return self.__add__(other)
-
-    def __mult__(self, other):
-        if not (isinstance(other, Matrix3d) or isinstance(other, Vec3d)):
-            return NotImplemented
-        pass
-
-    def __rmult__(self, other):
-        if not isinstance(other, Matrix3d):
-            return NotImplemented
-        pass
-
--- tests/test_base_objects.py
+++ tests/test_base_objects.py
@@ -1,19 +1,122 @@
 import unittest
 
-from integrationutils.util.base_objects import Vec3d, Matrix3d
+from integrationutils.util.base_objects import Matrix
+from integrationutils.util.base_objects import Vec3d
+
+class Test_Matrix(unittest.TestCase):
+
+    def test_constructor(self):
+        pass
+
+    def test_fill(self):
+        A = Matrix.fill(2, 2, 1.0)
+        B = Matrix([[1,1],[1,1]])
+        for i in range(2):
+            for j in range(2):
+                self.assertTrue(A[i,j] == B[i,j], f"Matrix element [{i},{j}] not equal.")
+        with self.assertRaises(ValueError):
+            Matrix.fill(-1,2,1)
+        with self.assertRaises(ValueError):
+            Matrix.fill(0,2,1)
+        with self.assertRaises(ValueError):
+            Matrix.fill(2,-1,1)
+        with self.assertRaises(ValueError):
+            Matrix.fill(2,0,1)
+
+    def test_zeros(self):
+        A = Matrix.zeros(2,3)
+        B = Matrix([[0,0,0],[0,0,0]])
+        C = Matrix.zeros(3)
+        D = Matrix([[0,0,0],[0,0,0],[0,0,0]])
+        for i in range(2):
+            for j in range(3):
+                self.assertTrue(A[i,j] == B[i,j], f"Matrix element [{i},{j}] not equal.")
+        for i in range(3):
+            for j in range(3):
+                self.assertTrue(C[i,j] == D[i,j], f"Matrix element [{i},{j}] not equal.")
+        with self.assertRaises(ValueError):
+            Matrix.zeros(-1,2)
+        with self.assertRaises(ValueError):
+            Matrix.zeros(0,2)
+        with self.assertRaises(ValueError):
+            Matrix.zeros(2,-1)
+        with self.assertRaises(ValueError):
+            Matrix.zeros(2,0)
+
+    def test_ones(self):
+        A = Matrix.ones(2,3)
+        B = Matrix([[1,1,1],[1,1,1]])
+        C = Matrix.ones(3)
+        D = Matrix([[1,1,1],[1,1,1],[1,1,1]])
+        for i in range(2):
+            for j in range(3):
+                self.assertTrue(A[i,j] == B[i,j], f"Matrix element [{i},{j}] not equal.")
+        for i in range(3):
+            for j in range(3):
+                self.assertTrue(C[i,j] == D[i,j], f"Matrix element [{i},{j}] not equal.")
+        with self.assertRaises(ValueError):
+            Matrix.ones(-1,2)
+        with self.assertRaises(ValueError):
+            Matrix.ones(0,2)
+        with self.assertRaises(ValueError):
+            Matrix.ones(2,-1)
+        with self.assertRaises(ValueError):
+            Matrix.ones(2,0)
+
+    def test_identity(self):
+        A = Matrix.identity(2)
+        B = Matrix([[1,0],[0,1]])
+        C = Matrix.identity(3)
+        D = Matrix([[1,0,0],[0,1,0],[0,0,1]])
+        for i in range(2):
+            for j in range(2):
+                self.assertTrue(A[i,j] == B[i,j], f"Matrix element [{i},{j}] not equal.")
+        for i in range(3):
+            for j in range(3):
+                self.assertTrue(C[i,j] == D[i,j], f"Matrix element [{i},{j}] not equal.")
+        with self.assertRaises(ValueError):
+            Matrix.identity(-1)
+        with self.assertRaises(ValueError):
+            Matrix.identity(0)
+
+    def test_equals(self):
+        A = Matrix([[1,2,3],[4,5,6]])
+        B = Matrix([[4,5,6],[7,8,9]])
+        C = Matrix([[1,2,3],[4,5,6]])
+        D = Matrix([[1,2,3],[4,5,6],[7,8,9]])
+        E = Matrix([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
+        self.assertTrue(A != B, "Matrices are not equal.")
+        self.assertTrue(A == C, "Matrices are equal.")
+        self.assertTrue(A != D, "Matrices are not equal.")
+        self.assertTrue(A != E, "Matrices are not equal.")
+
+    def test_add(self):
+        A = Matrix([[1,2,3],[4,5,6]])
+        B = Matrix([[4,5,6],[7,8,9]])
+        C = Matrix([[5,7,9],[11,13,15]])
+        D = Matrix([[-1,-2,-3],[-4,-5,-6]])
+        self.assertTrue(A + B == C, "The matrix sum was not computed successfully.")
+        self.assertTrue(B + A == C, "The matrix sum was not computed successfully.")
+        self.assertTrue(A + D == Matrix.zeros(2,3), "The matrix sum was not computed successfully.")
+        self.assertTrue(D + A == Matrix.zeros(2,3), "The matrix sum was not computed successfully.")
+        self.assertTrue(A + 3 == B, "The matrix sum was not computed successfully.")
+
+    # def test_subtract(self):
+    #     A = Matrix([[1,2,3],[4,5,6]])
+    #     B = Matrix([[4,5,6],[7,8,9]])
+    #     self.assertTrue(A - B == Matrix.fill(3,2,-3), "The matrix difference was not computed successfully.")
+    #     self.assertTrue(B - A == Matrix.fill(3,2,3), "The matrix difference was not computed successfully.")
 
 class Test_Vec3d(unittest.TestCase):
 
     def test_constructor(self):
         pass
 
-class Test_Matrix3d(unittest.TestCase):
-
-    def test_constructor(self):
-        pass
-
-    def test_fill(self):
-        print(Matrix3d.fill(1.0))
+    def test_add(self):
+        A = Vec3d(1,2,3)
+        B = Vec3d(4,5,6)
+        C = Vec3d(5,7,9)
+        self.assertTrue(A + B == C, "The vector sum was not computed successfully.")
 
 if __name__ == '__main__':
     unittest.main()
