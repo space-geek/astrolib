c784675539e2bde055b0502d27efc33872eb9f8f
David Black <david.black@colorado.edu>
2020-08-09 22:25:32 -0400

    Fixed matrix multiplication bug, added implementations for Vec3d methods

--- tests/test_base_objects.py
+++ tests/test_base_objects.py
@@ -118,12 +118,48 @@ class Test_Matrix(unittest.TestCase):
         self.assertTrue(A + D == Matrix.zeros(2,3), "The matrix sum was not computed successfully.")
         self.assertTrue(D + A == Matrix.zeros(2,3), "The matrix sum was not computed successfully.")
         self.assertTrue(A + 3 == B, "The matrix sum was not computed successfully.")
+        self.assertTrue(3 + A == B, "The matrix sum was not computed successfully.")
+        with self.assertRaises(TypeError):
+            A + "foo"
+        with self.assertRaises(TypeError):
+            "foo" + A
 
-    # def test_subtract(self):
-    #     A = Matrix([[1,2,3],[4,5,6]])
-    #     B = Matrix([[4,5,6],[7,8,9]])
-    #     self.assertTrue(A - B == Matrix.fill(3,2,-3), "The matrix difference was not computed successfully.")
-    #     self.assertTrue(B - A == Matrix.fill(3,2,3), "The matrix difference was not computed successfully.")
+    def test_subtract(self):
+        A = Matrix([[1,2,3],[4,5,6]])
+        B = Matrix([[4,5,6],[7,8,9]])
+        C = Matrix([[-2,-1,0],[1,2,3]])
+        D = Matrix([[2,1,0],[-1,-2,-3]])
+        self.assertTrue(A - B == Matrix.fill(2,3,-3), "The matrix difference was not computed successfully.")
+        self.assertTrue(B - A == Matrix.fill(2,3,3), "The matrix difference was not computed successfully.")
+        self.assertTrue(A - 3 == C, "The matrix difference was not computed successfully.")
+        self.assertTrue(3 - A == D, "The matrix difference was not computed successfully.")
+        with self.assertRaises(TypeError):
+            A - "foo"
+        with self.assertRaises(TypeError):
+            "foo" - A
+
+    def test_mult(self):
+        A = Matrix([[4,4,3,1,2],[2,9,8,1,6],[3,6,8,6,5],[7,6,4,8,1],[5,10,6,10,4]])
+        B = Matrix([[0.1206,0.2518,0.9827,0.9063,0.0225],[0.5895,0.2904,0.7302,0.8797,0.4253],[0.2262,0.6171,0.3439,0.8178,0.3127],[0.3846,0.2653,0.5841,0.2607,0.1615],[0.5830,0.8244,0.1078,0.5944,0.1788]])
+        C = Matrix([[5.0696,5.9343,8.6829,11.0466,3.2483],[11.2388,13.2658,12.5193,20.0984,7.6082],[10.9310,13.1484,14.1238,19.0751,6.9836],[8.9460,8.9203,17.4160,17.5733,5.4307],[14.0334,13.8163,20.5508,23.2193,8.5714]])
+        D = Matrix([[10.3908,14.3077,13.9979,13.7440,7.6617],[13.4135,18.8840,16.0042,16.5513,9.1536],[10.4585,16.5556,13.5137,12.5758,7.9429],[6.4538,10.6096,9.9605,7.8550,6.1879],[9.3583,15.7517,12.6561,8.5965,7.9605]])
+        E = Matrix([[31.1723,42.9230,41.9937,41.2320,22.9852],[40.2406,56.6520,48.0126,49.6538,27.4608],[31.3754,49.6667,40.5410,37.7274,23.8288],[19.3613,31.8289,29.8814,23.5650,18.5638],[28.0750,47.2552,37.9684,25.7895,23.8815]])
+        F = Matrix([[1,2,3],[4,5,6],[7,8,9]])
+        G = Matrix([[1,2],[3,4],[5,6]])
+        H = Matrix([[22,28],[49,64],[76,100]])
+        tol = 2.0e-3
+        self.assertTrue(abs(A * B - C) <= Matrix.fill(C.num_rows, C.num_cols, tol), "The matrix product was not computed successfully.")
+        self.assertTrue(abs(B * A - D) <= Matrix.fill(D.num_rows, D.num_cols, tol), "The matrix product was not computed successfully.")
+        self.assertTrue(abs(3 * D - E) <= Matrix.fill(E.num_rows, E.num_cols, tol), "The matrix product was not computed successfully.")
+        self.assertTrue(abs(D * 3 - E) <= Matrix.fill(E.num_rows, E.num_cols, tol), "The matrix product was not computed successfully.")
+        self.assertTrue(abs(F * G - H) <= Matrix.fill(H.num_rows, H.num_cols, tol), "The matrix product was not computed successfully.")
+        with self.assertRaises(ValueError):
+            G * F
+
+    def test_transpose(self):
+        A = Matrix([[1,2,3],[4,5,6]])
+        B = Matrix([[1,4],[2,5],[3,6]])
+        self.assertTrue(A.transpose() == B, "The matrix transpose was not computed successfully.")
 
 class Test_Vec3d(unittest.TestCase):
 
@@ -135,6 +171,16 @@ class Test_Vec3d(unittest.TestCase):
         B = Vec3d(4,5,6)
         C = Vec3d(5,7,9)
         self.assertTrue(A + B == C, "The vector sum was not computed successfully.")
+        self.assertTrue(B + A == C, "The vector sum was not computed successfully.")
+        self.assertTrue(A + 3 == B, "The vector sum was not computed successfully.")
+        self.assertTrue(3 + A == B, "The vector sum was not computed successfully.")
+
+    def test_mul_with_matrix(self):
+        x = Vec3d(1,2,3)
+        A = Matrix([[1,2,3],[4,5,6],[7,8,9]])
+        b = Vec3d(14,32,50)
+        self.assertTrue(Matrix.identity(3) * x == x, "The matrix multiplication with the vector was not computed successfully.")
+        self.assertTrue(A * x == b, "The matrix multiplication with the vector was not computed successfully.")
 
 if __name__ == '__main__':
     unittest.main()
