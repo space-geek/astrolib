ded0f2122a65e6a36ab6c26913676e1e5cc9462d
David Black <david.black@colorado.edu>
2020-09-04 13:03:10 -0400

    Added Euler, RK4, and RK45 integration implementations with required constant. Also added time-related constants, for upcoming commit on time objects

---
+++ src/integrationutils/integration/errors.py
@@ -0,0 +1,18 @@
+
+class NumericalIntegrationError(Exception):
+    """ Base exception type for errors encountered in numerical integration."""
+
+class MinimumStepSizeExceededError(NumericalIntegrationError):
+    """ Exception raised when integration step exceeds minimum step size.
+
+    Attributes:
+        h (float)       Attempted integration step size, in seconds.
+        h_min (float)   Minimum integration step size, in seconds.
+
+    """
+
+    def __init__(self, h_sec: float, h_min_sec: float):
+        self.message = f"Minimum integration step size exceeded. A {h_sec} second step was attempted with a minimum integration step size of {h_min_sec} seconds."
+        self.h = h_sec
+        self.h_min = h_min_sec
+        super().__init__(self.message)
