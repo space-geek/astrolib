56d1267a4bf2524a59ba7aaaea8107f99c6cc16e
David Black <david.black@colorado.edu>
2020-09-20 22:33:08 -0400

    Removed erroneous check on matrix dimensionality in element set construction, added stringify method to Quaternions, added additional validation to column matrix-based Matrix factory method

--- src/integrationutils/attitude_elements.py
+++ src/integrationutils/attitude_elements.py
@@ -15,8 +15,20 @@ class AttitudeElementSet(ElementSetBase):
 class Quaternion(AttitudeElementSet):
     """Class represents a quaternion representation of the attitude of an object, with q1, q2, and q3 as the vector components and q4 as the scalar"""
 
+    @classmethod
+    def identity(cls):
+        """Factory method to create a Quaternion representing a zero/identity rotation.
+
+        Returns:
+            Quaternion: The Quaternion representing the zero rotation.
+        """
+        return cls(x=0,y=0,z=0,w=1)
+
     def __init__(self, x: float, y: float, z: float, w: float):
         super().__init__([Matrix([[x],[y],[z],[w]])])
+
+    def __str__(self) -> str:
+        return f'[x = {self.x}, y = {self.y}, z = {self.z}, w = {self.w}]'
 
     @property
     def x(self) -> float:
