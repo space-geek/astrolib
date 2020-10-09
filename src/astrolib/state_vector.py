f13b1eb6bfeb5b989214f1ca2e603525833477f0
David Black <dblack10@vt.edu>
2020-10-09 17:01:26 -0400

    Committing incremental step in state vector update to support propagation implementation

--- src/astrolib/state_vector.py
+++ src/astrolib/state_vector.py
@@ -1,6 +1,8 @@
 from collections import OrderedDict
 from enum import IntEnum
 from typing import List
+from typing import Tuple
+from typing import Type
 
 from astrolib.base_objects import Matrix
 from astrolib.base_objects import TimeSpan
@@ -36,8 +38,26 @@ class StateVector():
         self.components = components
 
     @property
-    def component_types(self) -> List[ElementSetBase]:
-        return [x.__class__ for x in self.components]
+    def num_elements(self) -> int:
+        return sum([x.num_elements for x in self.components])
+
+    @property
+    def component_types(self) -> Tuple[Type]:
+        return tuple(x.__class__ for x in self.components)
+
+    @property
+    def component_indices(self) -> List[Tuple[int, int]]:
+        component_indices = []
+        start_ind = 0
+        for component in self.components:
+            end_ind = start_ind + component.num_elements - 1
+            component_indices.append((start_ind, end_ind))
+            start_ind = end_ind + 1
+        return component_indices
 
     def to_column_matrix(self) -> Matrix:
         return Matrix.from_column_matrices([x.to_column_matrix() for x in self.components])
+
+    def from_column_matrix(self, X: Matrix):
+        for start_ind, end_ind in self.component_indices:
+            print(start_ind, end_ind)
