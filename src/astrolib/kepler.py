6e1ffbb08f14a25601de5e8c2b0f4c96a2e145c9
David Black <dblack10@vt.edu>
2021-01-19 20:57:01 -0500

    Updated organization of orbit element sets, added constants file, added a few miscellaneous astro utility functions

---
+++ src/astrolib/kepler.py
@@ -0,0 +1,19 @@
+""" TODO Module docstring
+"""
+
+from astrolib.base_objects import Vec3d
+from astrolib.orbit_elements import OrbitElementSet
+from astrolib.constants import EARTH_MU
+
+
+def angular_momentum_vector(elements: OrbitElementSet) -> Vec3d:
+    #TODO inject orbit element conversion to cartesian elements
+    return elements.position.cross(elements.velocity)
+
+def eccentricity_vector(elements: OrbitElementSet, mu: float = EARTH_MU) -> Vec3d:
+    #TODO inject orbit element conversion to cartesian elements
+    return (1 / mu) * elements.velocity.cross(angular_momentum_vector(elements)) - elements.position.normalized()
+
+def specific_energy(elements: OrbitElementSet, mu: float = EARTH_MU) -> float:
+    #TODO inject orbit element conversion to cartesian elements
+    return (1 / 2) * pow(elements.velocity.norm(), 2) - mu * (1 / elements.position.norm())
