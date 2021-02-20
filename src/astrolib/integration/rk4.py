0e5bd62d6e79976ce7fba500729a78c4eeb5886c
David Black <dblack10@vt.edu>
2021-02-20 16:10:00 -0500

    Code cleanup in RK4 module

--- src/astrolib/integration/rk4.py
+++ src/astrolib/integration/rk4.py
@@ -1,17 +1,23 @@
+""" TODO: Module docstring
+"""
 from typing import Callable
 from typing import Tuple
 
-from astrolib.base_objects import Matrix
-from astrolib.base_objects import TimeSpan
+from astrolib import Matrix
+from astrolib import TimeSpan
 
 
-def integrate(t_0: TimeSpan, X_0: Matrix, h: TimeSpan, dynamics_func: Callable[[TimeSpan, Matrix],Matrix]) -> Tuple[TimeSpan, Matrix, TimeSpan]:
+def integrate(t_0: TimeSpan,
+              x_0: Matrix,
+              h: TimeSpan,
+              dynamics_func: Callable[[TimeSpan, Matrix],Matrix]
+              ) -> Tuple[TimeSpan, Matrix, TimeSpan]:
     """ Function utilizes a 4th-order Runge-Kutta integration scheme to integrate
         the input state vector from the initial to the final epoch.
 
     Arguments:
         t_0 (TimeSpan)                                      Epoch of the initial state.
-        X_0 (Matrix)                                        Initial state vector, in column matrix form.
+        x_0 (Matrix)                                        Initial state vector, in column matrix form.
         h (TimeSpan)                                        Step size for integration step to take.
         dynamics_func ([Callable[TimeSpan, Matrix],Matrix]) State vector dynamics function.
 
@@ -21,13 +27,13 @@ def integrate(t_0: TimeSpan, X_0: Matrix, h: TimeSpan, dynamics_func: Callable[[
     """
 
     # Compute the RK coefficients:
-    k1 = h.to_seconds() * dynamics_func(t_0, X_0)
-    k2 = h.to_seconds() * dynamics_func(t_0 + (1.0/2.0) * h, X_0 + (1.0/2.0) * k1)
-    k3 = h.to_seconds() * dynamics_func(t_0 + (1.0/2.0) * h, X_0 + (1.0/2.0) * k2)
-    k4 = h.to_seconds() * dynamics_func(t_0 + (    1.0) * h, X_0 + (    1.0) * k3)
+    k_1 = h.to_seconds() * dynamics_func(t_0, x_0)
+    k_2 = h.to_seconds() * dynamics_func(t_0 + (1.0/2.0) * h, x_0 + (1.0/2.0) * k_1)
+    k_3 = h.to_seconds() * dynamics_func(t_0 + (1.0/2.0) * h, x_0 + (1.0/2.0) * k_2)
+    k_4 = h.to_seconds() * dynamics_func(t_0 + (    1.0) * h, x_0 + (    1.0) * k_3)
 
     # Build up the new state vector:
     t_n = t_0 + h
-    X_n = X_0 + (1.0/6.0) * (k1 + 2.0 * k2 + 2.0 * k3 + k4)
+    x_n = x_0 + (1.0/6.0) * (k_1 + 2.0 * k_2 + 2.0 * k_3 + k_4)
 
-    return t_n, X_n, h
+    return t_n, x_n, h
