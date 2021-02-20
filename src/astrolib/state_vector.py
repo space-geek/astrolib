0b036474dd0c931f559ddc96007423b8586a24f6
David Black <dblack10@vt.edu>
2021-02-20 14:58:30 -0500

    Updated state vector and element set implementations in support of propagation scheme updates

--- src/astrolib/state_vector.py
+++ src/astrolib/state_vector.py
@@ -1,66 +1,87 @@
-from collections import OrderedDict
-from enum import IntEnum
+""" TODO: Module docstring
+"""
+from typing import Iterator
 from typing import List
 from typing import Tuple
 from typing import Type
 
-from astrolib.base_objects import Matrix
-from astrolib.base_objects import TimeSpan
-
-
-
-class ElementSetBase():
-    """ Class represents a set of generic state vector elements, e.g.
-    """
-
-    def __init__(self, elements: List[Matrix]):
-        self._elements = Matrix.from_column_matrices(elements)
-
-    @property
-    def num_elements(self) -> int:
-        return self._elements.num_rows
-
-    def to_column_matrix(self) -> Matrix:
-        return self._elements
-
-    def from_column_matrix(self, value: Matrix):
-        if not isinstance(value, Matrix) or value.num_cols != 1:
-            raise ValueError("Input value must be a column matrix.")
-        if value.num_rows != self.num_elements:
-            raise ValueError(f"Input column matrix must have {self.num_elements} elements.")
-        self._elements = value
-
-    def compute_time_derivative(self, epoch: TimeSpan) -> Matrix:
-        raise NotImplementedError()
+from astrolib import Matrix
+from astrolib import TimeSpan
+from astrolib import Vec3d
+from astrolib.base_objects import ElementSetBase
+from astrolib.orbit_elements import CartesianElements
+from astrolib.orbit_elements import OrbitElementSet
 
 
 class StateVector():
 
-    def __init__(self, epoch: TimeSpan=TimeSpan.undefined(), components: List[ElementSetBase]=[]):
+    @classmethod
+    def from_column_matrix(cls, epoch: TimeSpan, elements: Matrix):
+        raise NotImplementedError
+
+    def __init__(self, epoch: TimeSpan, components: List[ElementSetBase]):
         self.epoch = epoch
-        self.components = components
+        self._components = components
+
+    def __iter__(self) -> Iterator[ElementSetBase]:
+        for component in self._components:
+            yield component
 
     @property
     def num_elements(self) -> int:
-        return sum([x.num_elements for x in self.components])
+        return sum([x.num_elements for x in self._components])
 
     @property
     def component_types(self) -> Tuple[Type]:
-        return tuple(x.__class__ for x in self.components)
+        return tuple(type(x) for x in self._components)
 
     @property
     def component_indices(self) -> List[Tuple[int, int]]:
         component_indices = []
         start_ind = 0
-        for component in self.components:
+        for component in self._components:
             end_ind = start_ind + component.num_elements - 1
             component_indices.append((start_ind, end_ind))
             start_ind = end_ind + 1
         return component_indices
 
-    def to_column_matrix(self) -> Matrix:
-        return Matrix.from_column_matrices([x.to_column_matrix() for x in self.components])
+    @property
+    def rates(self) -> Matrix:
+        raise NotImplementedError
 
-    def from_column_matrix(self, X: Matrix):
-        for start_ind, end_ind in self.component_indices:
-            print(start_ind, end_ind)
+    def to_column_matrix(self) -> Matrix:
+        return Matrix.from_column_matrices([x.to_column_matrix() for x in self._components])
+
+
+class OrbitStateVector(StateVector):
+
+    def __init__(self, epoch: TimeSpan, elements: OrbitElementSet):
+        super().__init__(epoch, components=[elements])
+
+    @property
+    def elements(self) -> OrbitElementSet:
+        return self._components[0]
+
+    @elements.setter
+    def elements(self, value: OrbitElementSet):
+        self._components[0] = value
+
+
+class CartesianStateVector(OrbitStateVector):
+
+    @classmethod
+    def from_column_matrix(cls, epoch: TimeSpan, elements: Matrix) -> 'CartesianStateVector':
+        if len(elements) != 6:
+            raise ValueError(f"The input matrix is of length {len(elements)} but needs to be of length {CartesianElements.num_elements}")
+        return cls(epoch, CartesianElements(Vec3d(*elements[:3].get_col(0)),
+                                            Vec3d(*elements[3:].get_col(0))))
+
+    def __init__(self, epoch: TimeSpan = TimeSpan.zero(), elements: CartesianElements = CartesianElements()):
+        super().__init__(epoch, elements)
+
+    def __str__(self) -> str:
+        return f"Cartesian Orbit State Vector\n\tEpoch: {self.epoch}\n\tElements: {self.elements}"
+
+    @property
+    def rates(self) -> Matrix:
+        return self.elements.velocity
