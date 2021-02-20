0e001538fb0253dae4413291cf97de7d6805b397
David Black <dblack10@vt.edu>
2021-02-20 16:03:14 -0500

    Incorporated support for Euler, RK4, and RK45 propagation of orbit state vectors

--- src/astrolib/propagators.py
+++ src/astrolib/propagators.py
@@ -1,6 +1,18 @@
+""" TODO: Module docstring
+"""
+from typing import Callable
+from typing import Iterator
 from typing import List
+from typing import Tuple
+from typing import Type
 
+from astrolib.base_objects import Matrix
 from astrolib.base_objects import TimeSpan
+from astrolib.constants import EARTH_MU
+from astrolib.dynamics.dynamics_model import DynamicsModel
+from astrolib.integration.euler import integrate as integrate_euler
+from astrolib.integration.rk4 import integrate as integrate_rk4
+from astrolib.integration.rk45 import integrate as integrate_rk45
 from astrolib.state_vector import StateVector
 
 
@@ -10,27 +22,33 @@ class PropagatorBase():
     def __init__(self):
         pass
 
-    def get_state(self, epoch: TimeSpan, X_0: StateVector) -> StateVector:
-        raise NotImplementedError()
+    def get_state(self, epoch: TimeSpan, *args, **kwargs) -> StateVector:
+        raise NotImplementedError
 
-    def get_states(self, epochs: List[TimeSpan], X_0: StateVector):# -> Ephemeris:
-        raise NotImplementedError()
+    def get_states(self, epochs: List[TimeSpan], *args, **kwargs) -> 'Ephemeris':
+        raise NotImplementedError
 
 
 class Ephemeris(PropagatorBase):
-    
-    def __init__(self, states: List[StateVector] = []):
+
+    @classmethod
+    def empty(cls) -> 'Ephemeris':
+        """ Factory method to create an empty Ephemeris.
+        """
+        return Ephemeris([])
+
+    def __init__(self, states: List[StateVector]):
         super().__init__()
         self._states = {x.epoch : x for x in states}
-    
+
     def __str__(self) -> str:
         return f"[Ephemeris with:\n\tStart Epoch:\t\t{self.start_epoch.to_seconds()} seconds" \
                f"\n\tEnd Epoch:\t\t{self.end_epoch.to_seconds()} seconds" \
                f"\n\tDuration:\t\t{self.duration.to_seconds()} seconds" \
                f"\n\tNumber of States:\t{self.num_states}]"
 
-    def __iter__(self) -> StateVector:
-        for state in sorted(self._states.values(), lambda x: x.epoch):
+    def __iter__(self) -> Iterator[StateVector]:
+        for state in sorted(self._states.values(), key=lambda x: x.epoch):
             yield state
 
     @property
@@ -47,7 +65,13 @@ class Ephemeris(PropagatorBase):
 
     @property
     def duration(self) -> TimeSpan:
-        return (self.end_epoch - self.start_epoch) if (self.start_epoch.is_defined() and self.end_epoch.is_defined()) else TimeSpan.zero()
+        return (self.end_epoch - self.start_epoch) \
+            if (self.start_epoch.is_defined() and self.end_epoch.is_defined()) else TimeSpan.zero()
+
+    @property
+    def vector_epochs(self) -> Iterator[TimeSpan]:
+        for state in self:
+            yield state.epoch
 
     def add_state(self, state: StateVector):
         self._states[state.epoch] = state
@@ -59,44 +83,138 @@ class Ephemeris(PropagatorBase):
     def clear(self):
         self._states.clear()
 
+    #pylint: disable=arguments-differ
     def get_state(self, epoch: TimeSpan) -> StateVector:
+        state = None
         if epoch in self._states:
-            return self._states[epoch]
+            state = self._states[epoch]
         else:
-            #TODO Implement interpolation of state base on time
-            raise NotImplementedError()
+            #TODO Implement interpolation of state based on time
+            raise NotImplementedError
+        return state
 
-    def get_states(self, epochs: List[TimeSpan]):# -> Ephemeris:
-        states = [self.get_state(epoch) for epoch in epochs]
-        return Ephemeris(states=states)
+    def get_states(self, epochs: List[TimeSpan]) -> 'Ephemeris':
+        return Ephemeris(states=[self.get_state(epoch) for epoch in epochs])
 
 
 class Integrator(PropagatorBase):
-    
+
     def __init__(self):
         super().__init__()
+        self.dynamics_model = DynamicsModel()
 
+    #pylint: disable=arguments-differ
+    def get_state(self, epoch: TimeSpan, X_0: StateVector, *args, **kwargs) -> StateVector:
+        raise NotImplementedError
+
+    #pylint: disable=arguments-differ
+    def get_states(self, epochs: List[TimeSpan], X_0: StateVector, *args, **kwargs) -> Ephemeris:
+        sorted_epochs = sorted(epochs)
+        states = [X_0]
+        for epoch in sorted_epochs:
+            states.append(self.get_state(epoch, states[-1]))
+        return Ephemeris(states=states)
+
+    def propagate_to(self, epoch: TimeSpan, X_0: StateVector) -> StateVector:
+        return self.get_state(epoch, X_0)
+
+    def _evaluate_dynamics(self, epoch: TimeSpan, X: Matrix, state_vector_type: Type) -> Matrix:
+        return self.dynamics_model.evaluate(state_vector_type.from_column_matrix(epoch, X))
+
+
+class FixedStepIntegrator(Integrator):
+
+    def __init__(self,
+                 integration_func: Callable[[TimeSpan, Matrix, TimeSpan, Callable[[TimeSpan, Matrix], Matrix]],
+                                            Tuple[TimeSpan, Matrix, TimeSpan]],
+                 step_size: TimeSpan):
+        super().__init__()
+        self._integration_func = integration_func
+        self.step_size = step_size
+
+    #pylint: disable=arguments-differ
     def get_state(self, epoch: TimeSpan, X_0: StateVector) -> StateVector:
-        raise NotImplementedError()
+        self._check_time_step_validity(X_0.epoch, epoch)
+        state_vector_type = type(X_0)
+        t_n = X_0.epoch
+        x_n = X_0.to_column_matrix()
+        while t_n != epoch:
+            t_n, x_n, _ = self._integration_func(t_n, x_n, self.step_size,
+                                                 lambda t, x: self._evaluate_dynamics(t, x, state_vector_type))
+        return state_vector_type.from_column_matrix(epoch, x_n)
 
+    #pylint: disable=arguments-differ
+    #pylint: disable=useless-super-delegation
     def get_states(self, epochs: List[TimeSpan], X_0: StateVector) -> Ephemeris:
-        raise NotImplementedError()
+        return super().get_states(epochs, X_0)
+
+    def _check_time_step_validity(self, t_0: TimeSpan, t_f: TimeSpan) -> bool:
+        if not ((t_f - t_0).to_seconds() % self.step_size.to_seconds()) == 0.0:
+            raise ValueError("Unable to propagate to desired epoch using fixed-step " \
+                             "integrator with selected step size.")
 
 
-class Euler(Integrator):
-    
-    def __init__(self):
+class Euler(FixedStepIntegrator):
+
+    def __init__(self, step_size: TimeSpan):
+        super().__init__(integrate_euler, step_size)
+
+
+class RK4(FixedStepIntegrator):
+
+    def __init__(self, step_size: TimeSpan):
+        super().__init__(integrate_rk4, step_size)
+
+
+class VariableStepIntegrator(Integrator):
+
+    def __init__(self,
+                 integration_func: Callable[[TimeSpan, Matrix, TimeSpan, Callable[[TimeSpan, Matrix], Matrix]],
+                                            Tuple[TimeSpan, Matrix, TimeSpan]],
+                 relative_error_tolerance: float = 1.0e-12
+                 ):
         super().__init__()
+        self._integration_func = integration_func
+        self.rel_tol = relative_error_tolerance
+
+    #pylint: disable=arguments-differ
+    def get_state(self, epoch: TimeSpan, X_0: StateVector) -> StateVector:
+        state_vector_type = type(X_0)
+        t_n = X_0.epoch
+        x_n = X_0.to_column_matrix()
+        while t_n != epoch:
+            step_size = epoch - t_n
+            t_n, x_n, _ = self._integration_func(t_n, x_n, step_size,
+                                                 lambda t, x: self._evaluate_dynamics(t, x, state_vector_type),
+                                                 rel_tol=self.rel_tol)
+        return state_vector_type.from_column_matrix(epoch, x_n)
+
+    #pylint: disable=arguments-differ
+    #pylint: disable=useless-super-delegation
+    def get_states(self, epochs: List[TimeSpan], X_0: StateVector) -> Ephemeris:
+        return super().get_states(epochs, X_0)
 
 
-class RK4(Integrator):
-    
+class RK45(VariableStepIntegrator):
+
     def __init__(self):
-        super().__init__()
+        super().__init__(integrate_rk45)
 
 
-class RK45(Integrator):
-    
-    def __init__(self):
+class TwoBody(PropagatorBase):
+
+    def __init__(self, mu: float = EARTH_MU):
         super().__init__()
-    
+        self._mu = mu
+
+    #pylint: disable=arguments-differ
+    def get_state(self, epoch: TimeSpan, X_0: StateVector) -> StateVector:
+        raise NotImplementedError
+
+    #pylint: disable=arguments-differ
+    def get_states(self, epochs: List[TimeSpan], X_0: StateVector):# -> Ephemeris:
+        sorted_epochs = sorted(epochs, lambda x: x.epoch)
+        states = [X_0]
+        for epoch in sorted_epochs:
+            states.append(self.get_state(epoch, states[-1]))
+        return Ephemeris(states=states)
---
+++ tests/functional/test_propagators.py
@@ -0,0 +1,48 @@
+from astrolib import TimeSpan
+from astrolib import Vec3d
+from astrolib.constants import EARTH_MU
+from astrolib.dynamics.gravity import PointMassGravityModel
+from astrolib.propagators import Euler
+from astrolib.propagators import RK4
+from astrolib.propagators import RK45
+from astrolib.solar_system.motion_models import OriginFixedMotionModel
+from astrolib.state_vector import CartesianStateVector
+
+def main():
+
+    num_steps = 10
+
+    initial_state = CartesianStateVector()
+    initial_state.epoch = TimeSpan.from_days(1.0)
+    initial_state.elements.position = Vec3d(42164, 0, 0)
+    initial_state.elements.velocity = Vec3d(0, 3.0746, 0.01)
+    # initial_state.elements.position.x = 42164.0
+    # initial_state.elements.position.y = 0.0
+    # initial_state.elements.position.z = 0.0
+    # initial_state.elements.velocity.x = 0.0
+    # initial_state.elements.velocity.y = 3.0746
+    # initial_state.elements.velocity.z = 0.0
+
+    print(initial_state)
+
+    earth_gravity = PointMassGravityModel(EARTH_MU, OriginFixedMotionModel())
+
+    rk4 = RK4(TimeSpan.from_seconds(300))
+    rk4.dynamics_model.force_models.append(earth_gravity)
+    rk45 = RK45()
+    rk45.dynamics_model.force_models.append(earth_gravity)
+
+    ephem_rk4 = rk4.get_states([initial_state.epoch + i * rk4.step_size for i in range(0, num_steps)], initial_state)
+    ephem_rk45 = rk45.get_states(list(ephem_rk4.vector_epochs), initial_state)
+    
+    for i, (s_rk4, s_rk45) in enumerate(zip(ephem_rk4, ephem_rk45)):
+        print(f"State Diff #{i+1}:")
+        print(f"Epoch:    {s_rk45.epoch - s_rk4.epoch}")
+        print(f"Position: {s_rk45.elements.position - s_rk4.elements.position}")
+        print(f"Velocity: {s_rk45.elements.velocity - s_rk4.elements.velocity}")
+
+    print(ephem_rk4)
+    print(ephem_rk45)
+
+if __name__ == "__main__":
+    main()
