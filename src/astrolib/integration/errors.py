4244428e6abba164bd7a5d8658e2abb96298e7de
David Black <dblack10@vt.edu>
2021-02-20 19:36:05 -0500

    Code cleanup in integration errors module

--- src/astrolib/integration/errors.py
+++ src/astrolib/integration/errors.py
@@ -1,10 +1,12 @@
-
+""" TODO: Module docstring
+"""
 
 class NumericalIntegrationError(Exception):
     """ Base exception type for errors encountered in numerical integration."""
 
+
 class MinimumStepSizeExceededError(NumericalIntegrationError):
-    """ Exception raised when integration step exceeds minimum step size.
+    """ Exception raised when an attempted integration step exceeds a minimum step size threshold.
 
     Attributes:
         h (float)       Attempted integration step size, in seconds.
@@ -13,7 +15,8 @@ class MinimumStepSizeExceededError(NumericalIntegrationError):
     """
 
     def __init__(self, h_sec: float, h_min_sec: float):
-        self.message = f"Minimum integration step size exceeded. A {h_sec} second step was attempted with a minimum integration step size of {h_min_sec} seconds."
+        self.message = f"Minimum integration step size exceeded. A {h_sec} second step was attempted with a minimum " \
+                       f"integration step size of {h_min_sec} seconds."
         self.h = h_sec
         self.h_min = h_min_sec
         super().__init__(self.message)
