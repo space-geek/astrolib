82273692e0c85c4d4da61956f1b87a900ee8d319
David Black <david.black@colorado.edu>
2021-08-17 22:02:36 -0400

    MAINT Cleaned up imports, added missing constants

--- tests/functional/test_propagators.py
+++ tests/functional/test_propagators.py
@@ -1,11 +1,11 @@
 from astrolib import TimeSpan
 from astrolib import Vec3d
-from astrolib.constants import EARTH_MU
 from astrolib.dynamics.gravity import PointMassGravityModel
 from astrolib.propagators import RK4
 from astrolib.propagators import RK45
 from astrolib.solar_system.motion_models import OriginFixedMotionModel
 from astrolib.state_vector import CartesianStateVector
+from astrolib.util.constants import EARTH_MU
 
 def main():
 
