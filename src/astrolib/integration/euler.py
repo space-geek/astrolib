4b52bf73ab98934b448e4c73f13577f52a1e12a2
David Black <dblack1021@gmail.com>
2023-03-19 09:23:57 -0400

    MAINT Updated Euler integrator to match RK signatures

--- src/astrolib/integration/euler.py
+++ src/astrolib/integration/euler.py
@@ -1,27 +1,34 @@
+""" TODO: Module docstring
+"""
 from typing import Callable
-from typing import Tuple
 
 from astrolib.base_objects import Matrix
-from astrolib.base_objects import TimeSpan
+from astrolib.integration import IntegratorResults
 
 
 def integrate(
-    t_0: TimeSpan,
-    X_0: Matrix,
-    h: TimeSpan,
-    dynamics_func: Callable[[TimeSpan, Matrix], Matrix],
-) -> Tuple[TimeSpan, Matrix, TimeSpan]:
+    t_0: float,
+    x_0: float | Matrix,
+    step_size: float,
+    dynamics_func: Callable[[float, float | Matrix], float | Matrix],
+) -> IntegratorResults:
     """Function utilizes a first-order/Euler integration scheme to integrate
-        the input state from the initial to the final epoch.
+    the input state from the initial to the final epoch.
 
     Arguments:
-        t_0 (TimeSpan)                                      Epoch of the initial state.
-        X_0 (Matrix)                                        Initial state vector, in column matrix form.
-        h (TimeSpan)                                        Step size for integration step to take.
-        dynamics_func ([Callable[TimeSpan, Matrix],Matrix]) State vector dynamics function.
+        t_0 (float): Epoch of the initial state.
+        x_0 (float | Matrix): Initial state vector, in column matrix form.
+        step_size (float): Step size for integration step to take.
+        dynamics_func (Callable[[float, float | Matrix], float | Matrix]): State vector dynamics function.
 
     Returns:
-        Tuple[TimeSpan, Matrix, TimeSpan]                   Tuple of the propagated state vector epoch, the
-                                                            propagated state vector, and the step size taken.
+        IntegratorResults: Tuple containing the integration results.
     """
-    return t_0 + h, X_0 + h.to_seconds() * dynamics_func(t_0, X_0), h
+    return IntegratorResults(
+        epoch=t_0 + step_size,
+        state=x_0 + step_size * dynamics_func(t_0, x_0),
+        total_step_seconds=step_size,
+        intermediate_step_seconds=[
+            step_size,
+        ],
+    )
--- tests/unit/integration/test_euler.py
+++ tests/unit/integration/test_euler.py
@@ -9,17 +9,20 @@ from astrolib.integration.euler import integrate
 
 class Test_Euler(unittest.TestCase):
     def test_simple(self):
-        t_0 = TimeSpan.zero()
-        X_0 = Matrix.ones(2, 1)
-        h = TimeSpan.from_seconds(1)
-        t_f, X_f, h_actual = integrate(
+        t_0 = 0.0
+        x_0 = Matrix.ones(2, 1)
+        h = 1.0
+        results = integrate(
             t_0,
-            X_0,
+            x_0,
             h,
-            lambda t, X: Matrix.fill(*X.size, 1.0),
+            lambda t, x: Matrix.fill(*x.size, 1.0),
         )
-        self.assertTrue(t_f == h, "Integration did not work.")
-        self.assertTrue(X_f == Matrix.fill(*X_0.size, 2.0), "Integration did not work.")
-        self.assertTrue(h_actual == h, "Integration did not work.")
+        self.assertTrue(results.epoch == h, "Integration did not work.")
+        self.assertTrue(
+            results.state == Matrix.fill(*x_0.size, 2.0),
+            "Integration did not work.",
+        )
+        self.assertTrue(results.total_step_seconds == h, "Integration did not work.")
 
     # TODO: Add more extensive Euler integration tests
